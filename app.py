5f11cb3624bb3d01c9d8ab616cce8a99bcfed5bd
Josh Yang <quickchicken02@gmail.com>
2023-02-15 21:03:20 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,60 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+# app.py
+# CSCI5117 Project 1
+# yang7182 
 
+#########################################################
+# Imports
+#########################################################
+from flask import *
+
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+# from db import db functions...
+
+#########################################################
+# App setup and authorization
+#########################################################
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app = Flask(__name__)
+app.secret_key = env.get('FLASK_SECRET')
+
+oauth = OAuth(app)
+oauth.register (
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+
+#########################################################
+# Regular routes
+#########################################################
+# Home page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+# Products page
+@app.route('/products', methods=["GET"])
+def products():
+  return render_template('products.html')
+
+# Routine page
+@app.route('/routine', methods=["GET"])
+def routine():
+  return render_template('routine.html')
+
+
